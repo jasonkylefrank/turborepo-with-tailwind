c683135d7acbac666d2a4acaf424b13d3074b4bc
Jason K Frank <jasonkylefrank@users.noreply.github.com>
2023-04-03 13:29:22 -0400

    Fixed the "Type error: Cannot find module 'ui' or its corresponding type declarations." by adding a "main" property in package.json

--- packages/ui/src/Button.tsx
+++ packages/ui/src/Button.tsx
@@ -3,8 +3,7 @@ import styles from "./Button.module.css";
 
 export const Button = () => {
   return (
-    // <div className={`rounded-md ${styles.root}`}>
-    <div className={`rounded-md`}>
+    <div className={`rounded-md ${styles.root}`}>
       This is new text.
       <a href="https://turbo.build/repo/docs">
         <div className="ui-flex ui-w-full ui-items-center ui-justify-center ui-rounded-md ui-border ui-border-transparent ui-px-8 ui-py-3 ui-text-base ui-font-medium ui-no-underline ui-bg-white ui-text-black hover:ui-bg-gray-300 md:ui-py-3 md:ui-px-10 md:ui-text-lg md:ui-leading-6">
