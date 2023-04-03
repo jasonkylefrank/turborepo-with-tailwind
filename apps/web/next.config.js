5145004188187759d0fc284764ad200bcdf56736
Jason K Frank <jasonkylefrank@users.noreply.github.com>
2023-04-03 12:57:55 -0400

    Working my way toward the transpilePackages approach... Current issues: (1) The Tailwind styles from the UI package are currently not getting applied in the consuming apps. (2) The production build fails with the "Type error: Cannot find module 'ui' or its corresponding type declarations.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  // Tells Next.js to automatically transpile dependencies from local packages (like in monorepos),
+  //  or from external dependencies (from node_modules), if specified.  See: https://nextjs.org/docs/advanced-features/compiler#module-transpilation
+  transpilePackages: ["ui"], // "ui" is my local library within the monorepo.
 };
